67e65feb92eac90616522f7ef1e821da5bad795e
raulibinceanu-ship-it <raulibinceanu@gmail.com>
2025-11-18 17:19:50 +0100

    finito e pulito!

--- src/AllTheBooks.jsx
+++ src/AllTheBooks.jsx
@@ -4,13 +4,19 @@ import { Card, Col, Row, Container } from "react-bootstrap";
 const AllTheBooks = () => {
   return (
     <Container className="mt-4">
-      <Row>
+      <Row className="g-3">
         {fantasy.map((book) => (
-          <Col xs={12} md={4} lg={3} key={book.asin} className="mb-4">
-            <Card>
-              <Card.Img variant="top" src={book.img} />
+          <Col xs={12} sm={6} md={4} lg={3} key={book.asin}>
+            <Card className="h-100">
+              <Card.Img
+                variant="top"
+                src={book.img}
+                style={{ height: "300px", objectFit: "cover" }}
+              />
               <Card.Body>
-                <Card.Title>{book.title}</Card.Title>
+                <Card.Title style={{ fontSize: "15px" }}>
+                  {book.title}
+                </Card.Title>
               </Card.Body>
             </Card>
           </Col>
