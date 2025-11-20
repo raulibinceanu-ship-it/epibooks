51ee42860b43a58edb92da17a610d6d03d6ab8df
raulibinceanu-ship-it <raulibinceanu@gmail.com>
2025-11-20 13:10:03 +0100

    Update SingleBook.jsx

--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -2,29 +2,43 @@ import { Component } from "react";
 import { Card } from "react-bootstrap";
 
 class SingleBook extends Component {
-  state = {
-    selected: false,
-  };
-
-  toggleSelected = () => {
-    this.setState({ selected: !this.state.selected });
-  };
+  state = { selected: false };
 
   render() {
     const book = this.props.book;
 
     return (
       <Card
-        className="mb-3"
-        onClick={this.toggleSelected}
+        className="mb-4 shadow-sm"
+        onClick={() => this.setState({ selected: !this.state.selected })}
         style={{
           border: this.state.selected ? "3px solid red" : "1px solid lightgray",
+          width: "100%",
+          height: "100%",
           cursor: "pointer",
         }}
       >
-        <Card.Img variant="top" src={book.img} />
+        <div
+          style={{
+            height: "300px",
+            overflow: "hidden",
+          }}
+        >
+          <Card.Img
+            variant="top"
+            src={book.img}
+            style={{
+              width: "100%",
+              height: "100%",
+              objectFit: "cover",
+            }}
+          />
+        </div>
+
         <Card.Body>
-          <Card.Title>{book.title}</Card.Title>
+          <Card.Title style={{ fontSize: "1rem", minHeight: "48px" }}>
+            {book.title}
+          </Card.Title>
         </Card.Body>
       </Card>
     );
