c7cdaac8c359a6597df5c26c2fc837265cfe0024
raulibinceanu-ship-it <raulibinceanu@gmail.com>
2025-11-24 14:26:11 +0100

    Update BookList.jsx

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,16 +1,48 @@
-import { Row, Col } from "react-bootstrap";
+import { Component } from "react";
+import { Container, Row, Col } from "react-bootstrap";
 import SingleBook from "./SingleBook";
+import CommentArea from "./CommentArea";
 
-function BookList(props) {
-  return (
-    <Row className="gy-4">
-      {props.books.map((book) => (
-        <Col xs={12} sm={6} md={4} lg={3} key={book.asin}>
-          <SingleBook book={book} />
-        </Col>
-      ))}
-    </Row>
-  );
+class BookList extends Component {
+  state = {
+    selectedAsin: null,
+  };
+
+  handleBookSelect = (asin) => {
+    this.setState({
+      selectedAsin: asin,
+    });
+  };
+
+  render() {
+    const books = this.props.books;
+
+    return (
+      <Container className="mt-4">
+        <Row>
+          {/* COLONNA SINISTRA: LIBRI */}
+          <Col md={8}>
+            <Row className="g-3">
+              {books.map((book) => (
+                <Col xs={12} sm={6} md={4} key={book.asin}>
+                  <SingleBook
+                    book={book}
+                    selectedAsin={this.state.selectedAsin}
+                    onBookSelect={this.handleBookSelect}
+                  />
+                </Col>
+              ))}
+            </Row>
+          </Col>
+
+          {/* COLONNA DESTRA: COMMENTI */}
+          <Col md={4}>
+            <CommentArea asin={this.state.selectedAsin} />
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
 }
 
 export default BookList;
