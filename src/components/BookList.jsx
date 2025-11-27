971a656164f364ba2f311c57e33281bc2f57fbb9
raulibinceanu-ship-it <raulibinceanu@gmail.com>
2025-11-27 13:05:05 +0100

    Update BookList.jsx

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,48 +1,40 @@
-import { Component } from "react";
+import { useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import SingleBook from "./SingleBook";
 import CommentArea from "./CommentArea";
 
-class BookList extends Component {
-  state = {
-    selectedAsin: null,
+function BookList({ books }) {
+  const [selectedAsin, setSelectedAsin] = useState(null);
+
+  const handleBookSelect = (asin) => {
+    setSelectedAsin(asin);
   };
 
-  handleBookSelect = (asin) => {
-    this.setState({
-      selectedAsin: asin,
-    });
-  };
+  return (
+    <Container className="mt-4">
+      <Row>
+        {/* COLONNA SINISTRA */}
+        <Col md={8}>
+          <Row className="g-3">
+            {books.map((book) => (
+              <Col xs={12} sm={6} md={4} key={book.asin}>
+                <SingleBook
+                  book={book}
+                  selectedAsin={selectedAsin}
+                  onBookSelect={handleBookSelect}
+                />
+              </Col>
+            ))}
+          </Row>
+        </Col>
 
-  render() {
-    const books = this.props.books;
-
-    return (
-      <Container className="mt-4">
-        <Row>
-          {/* COLONNA SINISTRA: LIBRI */}
-          <Col md={8}>
-            <Row className="g-3">
-              {books.map((book) => (
-                <Col xs={12} sm={6} md={4} key={book.asin}>
-                  <SingleBook
-                    book={book}
-                    selectedAsin={this.state.selectedAsin}
-                    onBookSelect={this.handleBookSelect}
-                  />
-                </Col>
-              ))}
-            </Row>
-          </Col>
-
-          {/* COLONNA DESTRA: COMMENTI */}
-          <Col md={4}>
-            <CommentArea asin={this.state.selectedAsin} />
-          </Col>
-        </Row>
-      </Container>
-    );
-  }
+        {/* COLONNA DESTRA */}
+        <Col md={4}>
+          <CommentArea asin={selectedAsin} />
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default BookList;
